8f022fa0c0859595d8bcb1f418bb1674d2121ee7
Richard Aviles <raviles@1005d-16.eecs.ku.edu>
2016-02-29 14:05:08 -0600

    Fix table for web store assignment

--- customerBackend.php
+++ customerBackend.php
@@ -8,14 +8,12 @@
 	
 	$name = $_POST["username"];
 	$pswrd = $_POST["password"];
-
-	echo("Your order has been processed.<br><br>Username: ".$name."<br>Password: ".$pswrd);
 	
-	if ($shipAmount == "0")
+	if ($shipAmount == 0)
 	{
             $ship = "Free 7 Day";
 	}
-	else if ($shipAmount == "50")
+	else if ($shipAmount == 50)
 	{
             $ship = "Over Night";
         }
@@ -23,49 +21,60 @@
         {
             $ship = "Three Day";
         }
-
-        echo "<table>";
+        
+        
+	echo("Your order has been processed.<br><br>Username: ".$name."<br>Password: ".$pswrd."<br><br><br><br>");
+	
+	echo "<table border='2'>";
 			
 
 			echo "<tr>";
-				echo "<th>"; echo "</th>";
-				echo "<th>"; echo "Quantity"; echo "</th>";
-				echo "<th>"; echo "Cost Per Item" echo "</th>";
-				echo "<th>"; echo "Sub Total"; echo "</th>";
+				echo "<td>"; echo "</td>";
+				echo "<td>"; echo "Quantity"; echo "</td>";
+				echo "<td>"; echo "Cost Per Item"; echo "</td>";
+				echo "<td>"; echo "Sub Total"; echo "</td>";
 			echo "</tr>";
 
-			echo "<tr>"
+			echo "<tr>";
 				echo "<td>"; echo "Toothpaste"; echo "</td>";
 				echo "<td>"; echo $pasteAmount; echo "</td>";
 				echo "<td>"; echo "$5.00"; echo "</td>";
-				echo "<td>"; echo ($pasteAmount*5); echo "</td>";
+				echo "<td>"; echo "$".($pasteAmount*5); echo "</td>";
 			echo "</tr>";
 
-			echo "<tr>"
+			echo "<tr>";
 				echo "<td>"; echo "Lip Balm"; echo "</td>";
 				echo "<td>"; echo $balmAmount; echo "</td>";
 				echo "<td>"; echo "$3.00"; echo "</td>";
-				echo "<td>"; echo ($balmAmount*3); echo "</td>";
+				echo "<td>"; echo "$".($balmAmount*3); echo "</td>";
 			echo "</tr>";
 
-			echo "<tr>"
+			echo "<tr>";
 				echo "<td>"; echo "Mints"; echo "</td>";
 				echo "<td>"; echo $mintAmount; echo "</td>";
 				echo "<td>"; echo "$8.00"; echo "</td>";
-				echo "<td>"; echo ($mintAmount*8); echo "</td>";
+				echo "<td>"; echo "$".($mintAmount*8); echo "</td>";
 			echo "</tr>";
 
-			echo "<tr>"
+			echo "<tr>";
 				echo "<td>"; echo "Shipping"; echo "</td>";
 				echo "<td>"; echo $ship; echo "</td>";
-				echo "<td>"; echo $shipAmount; echo "</td>";
+				echo "<td>"; echo "</td>";
+				echo "<td>"; echo "$".$shipAmount; echo "</td>";
 			echo "</tr>";
 
-			echo "<tr>"
+			echo "<tr>";
 				echo "<td>"; echo "Total Cost"; echo "</td>";
-				echo "<td>"; echo $total; echo "</td>";
+				echo "<td>"; echo "</td>";
+				echo "<td>"; echo "</td>";
+				echo "<td>"; echo "$".$total; echo "</td>";
 			echo "</tr>";
         echo "</table>";
+
+	
+	
+
+        
 	
 
 
