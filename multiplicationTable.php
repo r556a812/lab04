387adbadeb402e95342188eda04caf09ddfef0c3
Richard Aviles <raviles@1005d-16.eecs.ku.edu>
2016-02-29 14:59:55 -0600

    Fix table for multiplication

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -1,12 +1,17 @@
 <?php
+
+        $num = 0;
+        echo "<table>";
         for ($i=1; $i<=100; $i++)
         {
-            echo "<tr>".PHP_EOL;
+            echo("<tr>".PHP_EOL);
             
             for ($j=1; $j<=100; $j++)
             {
                 $num = $i*$j;
-                echo "<td>".$num."</td>".PHP_EOL;
+                echo("<td>".$num."</td>".PHP_EOL);
             }
+            echo "</tr>";
         }
+        echo "</table>";
 ?> 
